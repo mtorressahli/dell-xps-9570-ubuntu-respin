64b555d9d358dd9d9ee591d675ea14252568844a
Manuel Torres Sahli <mtorressahli@gmail.com>
2019-11-28 02:44:54 +0000

    Create xps-software.sh

---
+++ xps-software.sh
@@ -0,0 +1,60 @@
+#!/usr/bin/env bash
+
+# Define colors
+RED='\033[0;31m'
+GREEN='\033[0;32m'
+NC='\033[0m' # No Color
+
+release=$(lsb_release -c -s)
+
+# Check if the script is running under Ubuntu 18.04 Bionic Beaver
+if [ "$release" != "bionic" ] && [ "$release" != "disco" ] && [ "$release" != "eoan" ] ; then
+    >&2 echo -e "${RED}This script is made for Ubuntu 18.04/19.04/19.10!${NC}"
+    exit 1
+fi
+
+# Check if the script is running as root
+if [ "$EUID" -ne 0 ]; then
+    >&2 echo -e "${RED}Please run xps-tweaks as root!${NC}"
+    exit 2
+fi
+
+# Enable universe and proposed
+add-apt-repository -y universe
+apt -y update
+apt -y full-upgrade
+
+
+# Ask for installing Brave
+echo -e "${GREEN}Now you are installing: Brave, R, LaTeX, Sublime, Mailspring, plank${NC}"
+
+## for Brave
+apt -y install apt-transport-https curl
+curl -s https://brave-browser-apt-release.s3.brave.com/brave-core.asc | apt-key --keyring /etc/apt/trusted.gpg.d/brave-browser-release.gpg add -
+source /etc/os-release
+echo "deb [arch=amd64] https://brave-browser-apt-release.s3.brave.com/ $UBUNTU_CODENAME main" | tee /etc/apt/sources.list.d/brave-browser-release-${UBUNTU_CODENAME}.list
+
+# for NordVPN
+wget -qnc https://repo.nordvpn.com/deb/nordvpn/debian/pool/main/nordvpn-release_1.0.0_all.deb
+
+# for Zotero
+wget -qO- https://github.com/retorquere/zotero-deb/releases/download/apt-get/install.sh | bash
+
+# for Sublime
+apt -y install apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
+add-apt-repository "deb https://download.sublimetext.com/ apt/stable/"
+
+## Update and install all
+apt update
+apt -y install brave-browser plank libopenblas-base r-base r-base-dev calibre zotero sublime-text
+# nordvpn yet not able to install by command line
+
+# Ask for installing LaTeX
+echo -e "${GREEN}Do you wish to install LaTeX (i.e. texlive-full) now? (tip: it takes a long time)${NC}"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) atp -y install texlive-full; break;;
+        No ) break;;
+    esac
+done
